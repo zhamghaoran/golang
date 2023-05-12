02733b6ee99e3a9e7d060c667301fa9f2205b838
zhanghaoran <2017929959@qq.com>
2023-05-12 14:09:19 +0800

    "完成了对于文件读取的操作"

---
+++ fileRead.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+type faq struct {
+	num      int
+	FileName string
+}
+
+func main() {
+	counts := make(map[string]faq)
+	for _, filename := range os.Args[1:] {
+		data, err := os.ReadFile(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "dump3: %v\n", err)
+		}
+		for _, line := range strings.Split(string(data), "\n") {
+			counts[line] = faq{
+				num:      counts[line].num + 1,
+				FileName: filename,
+			}
+		}
+
+	}
+	for line, n := range counts {
+		if n.num > 1 {
+			fmt.Printf("%s  %d  %s\n", line, n.num, n.FileName)
+		}
+	}
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module learn
+
+go 1.19
+
+require (
+	golang.org/x/mod v0.10.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/tools v0.9.1 // indirect
+	gopl.io v0.0.0-20211004154805-1ae3ec64947b // indirect
+)
